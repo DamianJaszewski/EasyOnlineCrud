abec4f53a2ec8c497924a5ee71a8ae1c27446942
DamianJaszewski <damianjaszewski@gmail.com>
2025-01-05 11:56:40 +0100

    add navigation buttons

--- easyonlinecrud.client/src/pages/Home.jsx
+++ easyonlinecrud.client/src/pages/Home.jsx
@@ -1,6 +1,12 @@
+
+import { Link } from 'react-router-dom'
 function Home() {
     return (
-        <div>home</div>
+        <div>
+            <Link class='nav-link' to='/'>Backlog</Link>
+            <Link class='nav-link' to='/login'>Login</Link>
+            <Link class='nav-link' to='/register'>Register</Link>
+        </div>
     )
 }
 
