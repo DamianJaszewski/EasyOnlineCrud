c120005fa1f4ccd8d0e436bcf47d4e4671f1abf8
DamianJaszewski <damianjaszewski@gmail.com>
2025-01-17 23:16:56 +0100

    navigation hamburger close after action

--- easyonlinecrud.client/src/components/Navigation.jsx
+++ easyonlinecrud.client/src/components/Navigation.jsx
@@ -1,30 +1,31 @@
-
+﻿
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from 'react-router-dom'
-
+import { useState } from "react";
 
 function Navigation() {
+
+    // Dodaj stan do obsługi zwijania menu
+    const [expanded, setExpanded] = useState(false);
+
+    const handleToggle = () => setExpanded(!expanded); // Przełączanie rozwinięcia
+    const handleClose = () => setExpanded(false); // Zamykanie 
+
     return (
-        <Navbar variant="light" expand="lg" sticky="top">
+        <Navbar variant="light" expand="lg" sticky="top" expanded={expanded} >
             <Container>
                 {/* Logo po lewej stronie */}
                 <Navbar.Brand href="/">Twoja Aplikacja</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggle} />
                 <Navbar.Collapse id="basic-navbar-nav">
                     {/* Nawigacja z prawej strony */}
                     <Nav className="ms-auto">
-                        <Link class='nav-link' to='/'>Home</Link>
-                        <Link class='nav-link' to='/login'>Login</Link>
-                        <Link class='nav-link' to='/register'>Register</Link>
+                        <Link class='nav-link' to='/' onClick={handleClose}>Home</Link>
+                        <Link class='nav-link' to='/login' onClick={handleClose}>Account</Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
-        //<div style={{display: "flex", justifyContent: "space-between" } }>
-        //    <Link class='nav-link' to='/'>Backlog</Link>
-        //    <Link class='nav-link' to='/login'>Login</Link>
-        //    <Link class='nav-link' to='/register'>Register</Link>
-        //</div>
     )
 }
 
